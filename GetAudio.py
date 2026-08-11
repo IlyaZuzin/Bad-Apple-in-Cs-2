8a71a26ac25c2f2e97c34ac582dffe6fe21b0b5a
IDK <141745964+IlyaZuzin@users.noreply.github.com>
2026-08-12 00:23:29 +0300

    Update GetAudio.py

--- GetAudio.py
+++ GetAudio.py
@@ -4,13 +4,10 @@ from moviepy import VideoFileClip
 video_path = r"G:\Python\Bad Apple cs 2\Bad_Apple.mp4"
 output_audio_path = r"G:\Python\Bad Apple cs 2\test_audio.mp3"
 
-# Загружаем видеофайл
 video = VideoFileClip(video_path)
 
-# Извлекаем аудио и сохраняем его
 video.audio.write_audiofile(output_audio_path)
 
-# Закрываем файл, чтобы освободить ресурсы
 video.close()
 
 print("Аудио успешно извлечено!")
